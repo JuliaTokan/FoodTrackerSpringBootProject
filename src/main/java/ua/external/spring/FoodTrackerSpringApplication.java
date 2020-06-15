ede0043acf5a0072df22481c321156f0ba43d027
Julia Tokan <tokan.yulia@gmail.com>
2020-06-15 17:33:12 +0300

    Add internationalization

--- src/main/java/ua/external/spring/FoodTrackerSpringApplication.java
+++ src/main/java/ua/external/spring/FoodTrackerSpringApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.JstlView;
--- src/main/java/ua/external/spring/controller/InfoController.java
+++ src/main/java/ua/external/spring/controller/InfoController.java
@@ -1,18 +1,25 @@
 package ua.external.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Locale;
+
 import static ua.external.spring.util.сonst.Pages.*;
 
 
 @Controller
 public class InfoController {
+
     @RequestMapping(value = "/")
-    public String index(HttpSession session, Model model) {
+    public String index(HttpSession session, Model model, HttpServletRequest request) {
         return INDEX_PAGE;
     }
 
